5127db85148056560719f18e84e71328806036bc
abbidabbers <abenfield@smith.edu>
2022-03-31 17:46:54 -0400

    Made certain variables into lists

--- data-raw/pokedex.R
+++ data-raw/pokedex.R
@@ -5,4 +5,9 @@ pokedex <-
   "data-raw/pokedex.csv" %>%
   read_csv() %>%
   clean_names()
+pokedex$type <- map(map(pokedex$type, ~strsplit(.x, ",")), unlist)
+pokedex$ev_yield <- map(map(pokedex$ev_yield, ~strsplit(.x, ",")), unlist)
+pokedex$egg_group <- map(map(pokedex$egg_group, ~strsplit(.x, ",")), unlist)
+pokedex$type_defenses <- map(map(pokedex$type_defenses, ~strsplit(.x, ",")), unlist)
+pokedex$evo_order <- map(map(pokedex$evo_order, ~strsplit(.x, ",")), unlist)
 usethis::use_data(pokedex, overwrite = TRUE)
