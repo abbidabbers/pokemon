947b067e0ea7aea766b4d7efd83c394dcd9b118a
abbidabbers <abenfield@smith.edu>
2022-04-16 22:43:09 -0400

    Got a minimally viable dataframe

--- data-raw/pokedex.R
+++ data-raw/pokedex.R
@@ -12,6 +12,17 @@ dex_url <- "https://pokeapi.co/api/v2/pokemon/"
 
 dex <- map(pokemon$url, GET)
 
-dex2 <- map(dex, function(x) { rawToChar(x$content)} )
+dex2 <- map(dex, function(x) {
+  rawToChar(x$content)
+})
 
 pokedex <- map(dex2, fromJSON)
+
+pokedex2 <- pokedex
+
+pokemon2 <- pokemon
+
+pokemon2$data <- pokedex2
+
+pokemon2 <- pokemon2 %>%
+  unnest_wider(data, names_repair = "minimal")
