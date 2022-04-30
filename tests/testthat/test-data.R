7e776bd33cb490cbf4863b1c6fcb2d92f0fcca71
kaceyjj <kjeanjacques@smith.edu>
2022-04-30 19:50:22 -0400

    Wrote test to confirm the correct number of rows and columns in the pokedex, ran the tests and passed.

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,4 +1,4 @@
 test_that("pokedex has correct number of rows and columns", {
-  expect_equal(nrow(pokedex), 24)
-  expect_equal(ncol(pokedex), 28)
+  expect_equal(nrow(pokedex), 1126)
+  expect_equal(ncol(pokedex), 33)
 })
