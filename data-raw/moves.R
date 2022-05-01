c7bd3c37df194571570e1dc92b246659e435009a
kaceyjj <kjeanjacques@smith.edu>
2022-04-30 21:09:28 -0400

    removed id column in moves_index to avoid duplication of generation; renamed "item_name" column to "move_name"; moves_index.rda file should appear in the data folder

--- data-raw/moves.R
+++ data-raw/moves.R
@@ -18,18 +18,17 @@ moves_index <- moves_raw %>%
   select(url) %>%
   add_column(data = dex) %>%
   unnest_wider(data, names_repair = "minimal") %>%
-  select(id, name, accuracy, pp, damage_class, power, learned_by_pokemon, generation) %>%
-  rename_with(~ c("id", "item_name", "accuracy", "pp", "damage_class", "power", "learned_by_pokemon", "generation"), names(.))
+  select(name, accuracy, pp, damage_class, power, learned_by_pokemon, generation) %>%
+  rename_with(~ c("move_name", "accuracy", "pp", "damage_class", "power", "learned_by_pokemon", "generation"), names(.))
 
 # Expand variable
-moves_index$id <- map(1:844, ~ moves_index$id[[.]][[1]])
-moves_index$id <- map(1:844, ~ moves_index$item_name[[.]][[1]])
-moves_index$id <- map(1:844, ~ moves_index$accuracy[[.]][[1]])
-moves_index$id <- map(1:844, ~ moves_index$pp[[.]][[1]])
-moves_index$id <- map(1:844, ~ moves_index$damage_class[[.]][[1]])
-moves_index$id <- map(1:844, ~ moves_index$power[[.]][[1]])
-moves_index$id <- map(1:844, ~ moves_index$learned_by_pokemon[[.]][[1]])
-moves_index$id <- map(1:844, ~ moves_index$generation[[.]][[1]])
+moves_index$move_name <- map(1:844, ~ moves_index$move_name[[.]][[1]])
+moves_index$accuracy <- map(1:844, ~ moves_index$accuracy[[.]][[1]])
+moves_index$pp <- map(1:844, ~ moves_index$pp[[.]][[1]])
+moves_index$damage_class <- map(1:844, ~ moves_index$damage_class[[.]][[1]])
+moves_index$power <- map(1:844, ~ moves_index$power[[.]][[1]])
+moves_index$learned_by_pokemon <- map(1:844, ~ moves_index$learned_by_pokemon[[.]][[1]])
+moves_index$generation <- map(1:844, ~ moves_index$generation[[.]][[1]])
 
 # Export data
 usethis::use_data(moves_index, overwrite = TRUE)
